744ffee28507d53369096456103a410849207df7
Steph Stone <hellostephstone@gmail.com>
2025-09-22 17:58:15 -0400

    refactoring

--- utils/fixtures.ts
+++ utils/fixtures.ts
@@ -6,8 +6,9 @@ export type TestOptions = {
 }
 
 export const test = base.extend<TestOptions>({
-	api: async ({}, use) => {
-		const requestHandler = new RequestHandler()
+	api: async ({ request }, use) => {
+		const baseUrl = 'https://conduit-api.bondaracademy.com/api'
+		const requestHandler = new RequestHandler(request, baseUrl)
 		await use(requestHandler)
 	},
 })
